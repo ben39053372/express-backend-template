02b467251c3d7684212edd43478699fff32550ee
test <test@test.com>
2019-11-27 12:41:27 +0800

    ..

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,10 @@
 const expressJwt = require('express-jwt');
 const middleware = require('express').Router();
+// import middleware
 const prefix = require('./prefix');
 const tokenHandler = require('./tokenHandler');
 
+// some middleware here
 middleware.use(prefix);
 middleware.use(tokenHandler);
 
